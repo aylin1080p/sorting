3dc0a62115580c26bc96f1c78195526a302db02d
aylin gunes <4ylingunes@gmail.com>
2023-01-25 14:09:20 +0100

    create a protocol and check the same numbers with it

--- sorting.playground/Contents.swift
+++ sorting.playground/Contents.swift
@@ -11,6 +11,8 @@ class ClassNotes {
         self.notes = notes
     }
     
+   
+    
 }
 
 var cN1 = ClassNotes(classes: "Math", notes: 45)
@@ -82,5 +84,48 @@ for p in personelArray{
     
 }
 
+/// set functions
+///
+class Student: Hashable {
+    var number:Int?
+    var name:String?
+    var classs:String?
+    
+    init(number:Int,name:String,classs:String) {
+        self.name = name
+        self.number = number
+        self.classs = classs
+    }
+    // add a protocol
+    // create a protocol to compare by hashValue - the students dont have the same schollNumber. this protocol controls this situation
+    var hashValue:Int {
+        get {
+            return number.hashValue
+        }
+    }
+    
+    static func == (lhs:Student, rhs:Student)->Bool { // this function waits two different Student objects and uses their number
+        return lhs.number == rhs.number // it can compare two different number and returns a boolean result using them
+        
+    }
+}
 
+var s1 = Student(number: 001, name: "ayşe", classs: "11B")
+var s2 = Student(number: 002, name: "ayşe", classs: "11B")
+var s3 = Student(number: 003, name: "ayşe", classs: "11B")
+var s4 = Student(number: 004, name: "ayşe", classs: "11B")
+var s5 = Student(number: 005, name: "ayşe", classs: "11B")
+
+
+var studentList = Set<Student>() // there is a problem because of the protocols. you should generate a protocol that should show how to use Student class ----hashable protocol----
+
+studentList.insert(s1)
+studentList.insert(s2)
+studentList.insert(s3)
+studentList.insert(s4)
+studentList.insert(s5)
+
+for ii in studentList {
+    print("student numbers : \(ii.number)")
+}
 
